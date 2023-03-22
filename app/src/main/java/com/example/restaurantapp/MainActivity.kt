d80618d777242076230662be88a8968ae6d3973e
Anvil974 <albertoaguilar2001@gmail.com>
2023-03-22 18:37:47 -0400

    Splash screen code ready for pictures

--- app/src/main/java/com/example/restaurantapp/MainActivity.kt
+++ app/src/main/java/com/example/restaurantapp/MainActivity.kt
@@ -1,16 +1,47 @@
 package com.example.restaurantapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.button
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.restaurantapp.databinding.ActivityMainBinding
+import org.w3c.dom.Text
+
+
+// Contains all the views
+@SuppressLint("StaticFieldLeak")
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //ssdsdsdsdada
-        //ssassasee
 
-        //I commented
-        //ssdsadsd
+        // Use Data Binding to get reference to the views
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.displayAddressButton.setOnClickListener {
+                // Code here executes on main thread after user presses button
+            if(binding.Address.visibility == View.GONE) {
+                binding.Address.visibility = View.VISIBLE
+            } else {
+                binding.Address.visibility = View.GONE
+            }
+
+        }
+
+        binding.displayHoursButton.setOnClickListener {
+            if(binding.Hours.visibility == View.GONE) {
+                binding.Hours.visibility = View.VISIBLE
+            } else {
+                binding.Hours.visibility = View.GONE
+            }
+        }
     }
+
+
+
 }
