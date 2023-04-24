1cb746406684e560269455bf35f37c8be0cc3bcd
Anvil974 <albertoaguilar2001@gmail.com>
2023-04-24 13:13:54 -0400

    Can now swap between screen fragments

--- app/src/main/java/com/example/restaurantapp/MainActivity.kt
+++ app/src/main/java/com/example/restaurantapp/MainActivity.kt
@@ -1,30 +1,47 @@
 package com.example.restaurantapp
 
-import android.R.attr.button
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.example.restaurantapp.databinding.ActivityMainBinding
-import org.w3c.dom.Text
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
-// Contains all the views
-@SuppressLint("StaticFieldLeak")
-private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var bottomNav: BottomNavigationView
+    lateinit var binding: ActivityMainBinding
+    private val titleFragment = TitleScreen()
+    private val menuFragment = MenuFragment()
+    private val socialFragment = SocialFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Use Data Binding to get reference to the views
-        //binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        /* Will be moved at some point
+        loadFragment(titleFragment)
+        bottomNav = findViewById(R.id.bottom_navigation) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.page_1 -> {
+                    loadFragment(titleFragment)
+                    true
+                }
+                R.id.page_2 -> {
+                    loadFragment(menuFragment)
+                    true
+                }
+                R.id.page_3 -> {
+                    loadFragment(socialFragment)
+                    true
+                }
+                else -> {false}
+            }
+
+            // Use Data Binding to get reference to the views
+            //binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+            /* Will be moved at some point
         binding.displayAddressButton.setOnClickListener {
                 // Code here executes on main thread after user presses button
             if(binding.Address.visibility == View.GONE) {
@@ -43,7 +60,14 @@ class MainActivity : AppCompatActivity() {
             }
         }
          */
+        }
+
+
     }
 
-
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
+    }
 }
